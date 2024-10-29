7c83ddecf45f02efd328983183975f6dd1190f35
Gabriel Klyatis <gabrielklyatis@gmail.com>
2024-10-29 19:48:35 +0200

    Cleanup of files

--- netlab_testing/pch.h
+++ netlab_testing/pch.h
@@ -4,26 +4,26 @@
 
 #pragma once
 
-#include "gtest/gtest.h"
-#include "../netlab/infra/inet_os.hpp"
-#include "../netlab/L1/NIC_Cable.h"
-#include "../netlab/L0/L0_buffer.h"
-#include "../netlab/L1/NIC.h"
-#include "../netlab/L2/L2.h"
-#include "../netlab/L2/L2_ARP.h"
-#include "../netlab/L4/L4.h"
-#include "../netlab/L4/L4_TCP.h"
-#include "../netlab/infra/HWAddress.hpp"
-#include "../netlab/L4/L4_UDP_Impl.hpp"
-
-#include "../netlab/L4/tcp_tahoe.h"
-#include "../netlab/L4/tcp_reno.h"
-
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <string>
-#include <cstdlib>
-#include <pthread.h>
-#include <thread>
-#include <chrono>
+//#include "gtest/gtest.h"
+//#include "../netlab/infra/inet_os.hpp"
+//#include "../netlab/L1/NIC_Cable.h"
+//#include "../netlab/L0/L0_buffer.h"
+//#include "../netlab/L1/NIC.h"
+//#include "../netlab/L2/L2.h"
+//#include "../netlab/L2/L2_ARP.h"
+//#include "../netlab/L4/L4.h"
+//#include "../netlab/L4/L4_TCP.h"
+//#include "../netlab/infra/HWAddress.hpp"
+//#include "../netlab/L4/L4_UDP_Impl.hpp"
+//
+//#include "../netlab/L4/tcp_tahoe.h"
+//#include "../netlab/L4/tcp_reno.h"
+//
+//#include <iostream>
+//#include <iomanip>
+//#include <vector>
+//#include <string>
+//#include <cstdlib>
+//#include <pthread.h>
+//#include <thread>
+//#include <chrono>
